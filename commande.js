dd959978b6654458b7b8683b484757a15554c18c
Julien Kerdaffret <julienkerdaffret@pc55.home>
2020-06-28 14:51:31 +0200

    ajout commande

--- commande.js
+++ commande.js
@@ -3,9 +3,17 @@
 const read = function () { 
     return responsiveVoice.speak("Bonjour Julien", "French Female");
 }
+const read2 = function () { 
+    return responsiveVoice.speak("Oui ?", "French Female");
+}
+const read3 = function () { 
+    return responsiveVoice.speak("Bien et vous ?", "French Female");
+}
 
 let commands = {               
     'salut': read,
     'bonjour': read,
-    'jarvis': read,
+    'jarvis': read2,
+    'Comment vas-tu ?': read3,
+    'Comment allez-vous ?': read3,
 };
