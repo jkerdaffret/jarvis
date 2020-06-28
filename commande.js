cb351a296d34a8a70c851e058cd2ddef076e5d3d
Julien Kerdaffret <julienkerdaffret@pc55.home>
2020-06-28 15:13:34 +0200

    test var annyang

--- commande.js
+++ commande.js
@@ -13,6 +13,19 @@ const google = function () {
     window.open('https://www.google.com/?hl=fr')
 }
 
+var showFlickr = function(tag) {
+    var url = 'http://api.flickr.com/services/rest/?tags='+tag;
+    $.getJSON(url);
+}
+      
+var calculateStats = function(month) {
+    $('#stats').text('Statistics for '+month);
+}
+      
+var greeting = function() {
+    $('#greeting').text('Hello!');
+}
+
 let commands = {               
     'salut': read,
     'bonjour': read,
@@ -20,4 +33,8 @@ let commands = {
     'Comment vas-tu ?': read3,
     'Comment allez-vous ?': read3,
     'Ouvre Google': google,
+    'montre-moi *tag': showFlickr,
+    'calculate :month stats': calculateStats,
+    'say hello friend': greeting
 };
+
